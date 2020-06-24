d405b3bc9b5bd91ded2434da368be8b604484b6f
jeromed <jeromed@n-compass.biz>
2020-06-24 15:10:50 +0800

    change port

--- consumer.js
+++ consumer.js
@@ -8,8 +8,8 @@ const io_client = require('socket.io-client');
 const io = io_client.connect('http://192.168.100.13:3000');
 const app = express();
 
-const server = app.listen(5001,function(){
-    console.log('Kafka consumer running at 5001')
+const server = app.listen(3000,function(){
+    console.log('Kafka consumer running at 3000')
 })
 
 // Start from latest offset.
