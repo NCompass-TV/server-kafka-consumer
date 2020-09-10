d7afe2212159bb24ea17e5363d0944b2df67206c
NCompass-TV <56720009+NCompass-TV@users.noreply.github.com>
2020-09-10 15:50:55 +0800

    Consumer ENV

--- consumer.js
+++ consumer.js
@@ -1,21 +1,29 @@
+require('dotenv').config();
+const PORT = process.env.PORT;
+const KAFKA_BROKER = process.env.KAFKA_BROKER;
+const API_URL = process.env.API_URL;
+const SOCKET_URL = process.env.SOCKET_URL;
 const axios = require('axios');
 const express = require('express');
 const kafka = require('kafka-node');
-var Consumer = kafka.Consumer;
-const client = new kafka.KafkaClient({kafkaHost: 'staging.n-compass.online:9092'});
-var offset = new kafka.Offset(client);
+const Consumer = kafka.Consumer;
+const client = new kafka.KafkaClient({kafkaHost: KAFKA_BROKER});
+const offset = new kafka.Offset(client);
 const io_client = require('socket.io-client');
-const io = io_client.connect('http://staging.n-compass.online:73');
+const io = io_client.connect(SOCKET_URL);
 const app = express();
 
-const server = app.listen(4000,function(){
-    console.log('Kafka consumer running at 4000')
+app.listen(PORT, () => {
+    console.log(`Consumer running at ${PORT}`)
+})
+
+io.on('connect', () => {
+    console.log('Connected to Socket Server');
 })
 
 // Start from latest offset.
 offset.fetch(
     [{ topic: 'contentPlayCount', partition: 0, time: -1, maxNum: 1 }], (err, data) => {
-        let count = 0;
         var consumer = new Consumer(
             client,
             [{topic: 'contentPlayCount', partition: 0, offset: data['contentPlayCount'][0][0]}],
@@ -34,10 +42,9 @@ offset.fetch(
                     contentId: log_data.content_id,
                     logDate: log_data.timestap
                 }];
-
-                const send_logs = await sendLogs(play_log);
+                const logs = await sendLogs(play_log);
                 io.emit('CS_content_log', play_log);
-                console.log('Logs Sent:', log_data);
+                console.log('Logs Sent Successfully:', logs, play_log);
             } catch(err) {
                 console.log('Invalid Log Format', message.value, err);
             }
@@ -47,7 +54,7 @@ offset.fetch(
 
 const sendLogs = (data) => {
     return new Promise((resolve, reject) => {
-        axios.post(`http://3.212.225.229:72/api/ContentPlays/Log`, data)
+        axios.post(`${API_URL}/ContentPlays/Log`, data)
         .then((res) => {
             resolve(res.status)
         }).catch((err) => {
@@ -57,5 +64,5 @@ const sendLogs = (data) => {
 }
 
 app.get('/ping', (req, res) => {
-    res.send('Consumer is up')
+    res.send('Consumer is up and running.')
 })
